6e24c33501a302d117e48132bbc23c31f1ff2e52
Jeff Stafford <jeff.stafford@live.com>
2016-03-06 00:26:42 -0500

    memory optimization in bg calc

--- pytrack.py
+++ pytrack.py
@@ -11,31 +11,29 @@ from matplotlib import widgets
 # configurable params
 search_size = 20
 per_pixel_threshold = 1.5
-arena_size = 8 # assumes square arena
+arena_size = 8  # assumes square arena
 
 
 def main(argv):
     path = 'Aarya_videos/IMG_5807.m4v'
+
+    # open video, grab basic metadata
     video = cv2.VideoCapture(path)
     if not video.isOpened():
         sys.exit('Could not open: ' + path)
     resolution = (video.get(cv2.CAP_PROP_FRAME_WIDTH), video.get(cv2.CAP_PROP_FRAME_HEIGHT))
     framerate = video.get(cv2.CAP_PROP_FPS)
-    # last frame of the video
     nfrm = int(video.get(cv2.CAP_PROP_FRAME_COUNT) - 1)
 
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
+    # grab region of interest
     ret, frame = video.read()
     roi = imrect(frame)
 
-    # bg = generate_background(video, 100)
-    # plt.imshow(bg, interpolation='nearest', cmap='magma')
-    # plt.colorbar()
-    # plt.show()
+    bg = generate_background(video, roi, 100)
+    plt.imshow(bg, interpolation='nearest', cmap='magma')
+    plt.colorbar()
+    plt.show()
 
-    # close reader
     video.release()
 
 
@@ -63,24 +61,27 @@ def imrect(image):
 
     selector = widgets.RectangleSelector(ax, nothing, drawtype='box', interactive=True)
     plt.connect('key_press_event', on_enter)
-    plt.show()
+    plt.show()  # blocks execution until on_enter() completes
     return bounds
 
 
-def generate_background(vreader, depth):
+def generate_background(vreader, bounds, depth):
     """
     Create a background image for use in subtraction
+
     :param vreader: an instance of cv2.VideoCapture()
+    :param bounds: bounds to crop the image with
     :param depth: number of frames to average
     :return: an averaged greyscale frame (as a numpy array)
     """
     print('Creating background', end='')
     randv = [random.randint(0, vreader.get(cv2.CAP_PROP_FRAME_COUNT - 1)) for i in range(0, depth)]
-    bg_array = np.zeros((vreader.get(cv2.CAP_PROP_FRAME_HEIGHT), vreader.get(cv2.CAP_PROP_FRAME_WIDTH), depth), 'uint8')
+    bg_array = np.zeros((bounds[3], bounds[2], depth), dtype='uint8')
     for frame in range(0, 100):
         print('.', end='')
-        status, gray = vreader.read(randv[frame])
-        bg_array[:, :, frame] = gray.mean(axis=2)
+        status, img = vreader.read(randv[frame])
+        plt.show()
+        bg_array[:, :, frame] = img[bounds[1]:bounds[1] + bounds[3], bounds[0]:bounds[0] + bounds[2], :].mean(axis=2)
     print('done!')
     return bg_array.mean(axis=2)
 
