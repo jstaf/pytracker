62b32cb6490a1b8b52957e470bc50f3473d8ce5f
Jeff Stafford <jeff.stafford@live.com>
2016-03-06 00:08:17 -0500

    added functional imrect equivalent

--- pytrack.py
+++ pytrack.py
@@ -6,11 +6,13 @@ import sys
 import random
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import widgets
 
 # configurable params
 search_size = 20
 per_pixel_threshold = 1.5
 arena_size = 8 # assumes square arena
+
 
 def main(argv):
     path = 'Aarya_videos/IMG_5807.m4v'
@@ -22,21 +24,47 @@ def main(argv):
     # last frame of the video
     nfrm = int(video.get(cv2.CAP_PROP_FRAME_COUNT) - 1)
 
-    # have a function to take user ROI input here
-    # unused for now, look into rectangle selection widget from matplotlib
-    # ret, frame = video.read()
-    # cv2.imshow('test', frame)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
 
-    bg = generate_background(video, 100)
+    ret, frame = video.read()
+    roi = imrect(frame)
 
-    plt.imshow(bg, interpolation='nearest', cmap='magma')
-    plt.colorbar()
-    plt.show()
+    # bg = generate_background(video, 100)
+    # plt.imshow(bg, interpolation='nearest', cmap='magma')
+    # plt.colorbar()
+    # plt.show()
 
     # close reader
     video.release()
+
+
+def imrect(image):
+    """
+    Best python hack of MATLAB's imrect that I could come up with
+
+    :param image: a numpy array
+    :return: returns the bounds of the selected roi
+    """
+    print('Click and drag to select ROI. Press "Enter" to proceed.')
+    fig = plt.figure('ROI selection')
+    plt.imshow(image)
+    ax = plt.axes()
+
+    def nothing(eclick, erelease):
+        pass
+
+    bounds = None
+    def on_enter(event):
+        if event.key == 'enter' and selector._rect_bbox != (0., 0., 0., 1.):
+            nonlocal bounds
+            bounds = [int(i) for i in selector._rect_bbox]
+            plt.close(fig)
+
+    selector = widgets.RectangleSelector(ax, nothing, drawtype='box', interactive=True)
+    plt.connect('key_press_event', on_enter)
+    plt.show()
+    return bounds
 
 
 def generate_background(vreader, depth):
